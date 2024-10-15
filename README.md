f69fb37760cde81417c1831fbdb3acfc8a7ca454
shubham mokal <shubham@gmail.com>
2024-10-15 20:04:23 +0530

    integrate with mlflow

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ML-project
+import dagshub
+dagshub.init(repo_owner='shubham3032002', repo_name='ML-project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -21,6 +21,7 @@ from xgboost import XGBRegressor
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from src.mlproject.utils import save_object,evaluate_models
+import dagshub
 
 
 @dataclass
@@ -30,6 +31,12 @@ class ModelTrainnerconfig:
 class ModelTrainner:
     def __init__(self):
         self.model_tariner_config =ModelTrainnerconfig()
+        
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2    
         
         
     def initate_model_trainer(self,train_array,test_array):
@@ -106,9 +113,54 @@ class ModelTrainner:
             print("This is the best model:")
             print(best_model_name)
             
+            
+            model_name=list(params.keys())
+            
+            actual_model=""
+             
+            for model in model_name:
+                 if best_model_name == model:
+                     actual_model =actual_model+model
+                  
+                  
+            best_params=params[actual_model]
+           
+            dagshub.init(repo_owner='shubham3032002', repo_name='ML-project', mlflow=True)
+            mlflow.set_registry_uri("https://dagshub.com/shubham3032002/ML-project.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            
+            #mlflow
+            
+            with mlflow.start_run():
+                
+                predicted_qualities=best_model.predict(X_test)
+                (rmse,mae,r2)=self.eval_metrics(y_test,predicted_qualities)     
+                
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)    
+                
+                
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+                
+            
             if best_model_score <0.6:
                 raise CustomException("No best model found")
             logging.info(f"best found model on both training and testing dataset ")
+            
+            
+            
             
             save_object(
                 file_path=ModelTrainnerconfig.trained_model_file_path,
@@ -117,6 +169,6 @@ class ModelTrainner:
             predicted=best_model.predict(X_test)
             r2_square =r2_score(y_test,predicted)
             
-            
+            return r2_square
         except Exception as e:
             raise CustomException(e,sys)        
